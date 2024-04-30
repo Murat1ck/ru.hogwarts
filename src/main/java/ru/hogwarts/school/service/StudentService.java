64a2777c6446873fb458f5b166b7ac8367050888
murat <muratoff101088@gmail.com>
2024-04-30 22:49:14 +0300

    SQL_4_2

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -34,7 +34,7 @@ public class StudentService {
 
 
     public Student findStudent(long id) {
-        return studentRepository.getById(id);
+        return studentRepository.findById(id).orElseThrow();
     }
 
     public Student editStudent(Student student) {
